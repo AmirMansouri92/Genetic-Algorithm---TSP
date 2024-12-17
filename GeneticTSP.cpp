7db634ad7160b073427deee3cb59e3947cf43e8a
AmirMansouri <Amir_mansouri92@yahoo.com>
2024-12-17 09:20:42 +0330

    modification

--- GeneticTSP.cpp
+++ GeneticTSP.cpp
@@ -2,7 +2,6 @@
 #include <fstream>
 #include <vector>
 #include <cmath>
-#include <string>
 using namespace std;
 
 struct Node {
@@ -48,28 +47,6 @@ class Individual {
         calculateFitness();
     }
 
-     /*void startNode (int startNode) {
-        fill (visited.begin(), visited.end(), false);
-        visited[startNode]=true;
-        genes[0]= startNode;
-        fitness = 0;
-    }*/
-
-    /*void createRandomRoute (){
-        int nextnode;
-        int nbselected=1; //the number of selected nodes at the beginning which is 1 (we have already chosen the first node of the route)
-        while (nbselected<NUM_NODES){
-            nextnode = (rand()%NUM_NODES);
-            if(visited [nextnode]!=true){
-                genes[nbselected] = nextnode;
-                visited[nextnode]=true;
-                nbselected++;
-            }
-        }
-
-        calculateFitness();
-    }*/
-
     vector <Node> generateNodes (){
         vector <Node> nodes (NUM_NODES);
         for (int i=0; i<NUM_NODES; i++){
@@ -106,13 +83,10 @@ class Individual {
         }
 
         double randnum=rand()/(double)RAND_MAX;
-        //cout<<"mutation rate is"<<MUTATION_RATE<<endl;
-        //cout<<"rand number is"<<randnum<<endl;
         if (randnum<MUTATION_RATE){
-            int k = genes [index_city1]; //k is used to keep the first random city
-            //cout<<"index city 1 is"<<index_city1<<"\t"<<"index city 2 is"<<index_city2;
+            int k = genes [index_city1]; 
             genes[index_city1] = genes[index_city2];
-            genes[index_city2] = k; //code lines 85 and 86 are used to exchange cities in one genes
+            genes[index_city2] = k; //code lines 88 and 89 are used to exchange cities in one gene
             calculateFitness();
         }
     }
@@ -127,10 +101,8 @@ class Individual {
             index_cut2=(rand()%(NUM_NODES-1))+1;
         }
         offspring1.genes[0] = genes[0]; // the beginning city doesn't change
-        //cout<<"index_cut1 = "<<index_cut1<<"\t"<<"index_cut2 = "<<index_cut2<<endl;
         for (int i=index_cut1; i<=index_cut2; i++){
             offspring1.genes[i] = genes[i];
-            //cout<<"offspring.genes["<<i<<"] = "<<offspring1.genes[i]<<"\t";
         }
         vector<int> intermediaryGene(NUM_NODES);
         vector<bool> canbeselected(NUM_NODES, true);
@@ -139,12 +111,10 @@ class Individual {
         int t=1;
         for(int i=index_cut2+1; i<NUM_NODES; i++){
             intermediaryGene[t]=other.genes[i];
-            //cout<<"intermediaryGene["<<t<<"] = "<<intermediaryGene[t]<<"\t";
             t++;
         }
         for(int i=1; i<index_cut2+1; i++){
             intermediaryGene[t]=other.genes[i];
-            //cout<<"intermediaryGene["<<t<<"] = "<<intermediaryGene[t]<<"\t";
             t++;
         }
        
@@ -152,7 +122,6 @@ class Individual {
             for(int j=1; j<NUM_NODES; j++){
                 if(offspring1.genes[i]==intermediaryGene[j]){
                     canbeselected[j]=false;
-                    //cout<<"canbeselected["<<j<<"] = "<<canbeselected[j]<<endl;
                 }
             }
        }
@@ -161,8 +130,6 @@ class Individual {
        for(int i=1; i<NUM_NODES; i++){
         if(canbeselected[i]==true){
             indexes[y]=intermediaryGene[i];
-            //cout<<"intermediary"<<i<<"="<<intermediaryGene[i]<<"\t";
-            //cout<<"indexes["<<y<<"]="<<indexes[y]<<"\t";
             y++;
         }
        }
@@ -172,19 +139,14 @@ class Individual {
        for(int i=index_cut2+1; i<NUM_NODES; i++){
                 offspring1.genes[i] = indexes[k];
                 k++;
-                //cout<<"offspring1.genes["<<i<<"] = "<<offspring1.genes[i]<<"\t";
        }
 
        for(int i=1; i<index_cut1; i++){
                 offspring1.genes[i] = indexes[k];
                 k++;
-                //cout<<"the begin of offspring1.genes["<<i<<"] = "<<offspring1.genes[i]<<"\t";
        }
 
-        /*for(int i=0; i<NUM_NODES; i++){
-            cout<<"offpring1.genes["<<i<<"]= "<<offspring1.genes[i]<<"\t";
-        }
-        cout<<endl;*/
+        
         return offspring1;
 
     }
@@ -207,7 +169,6 @@ class Population {
         for (const auto& x : individuals){
             if(x.fitness<bestIndividual.fitness){
                 bestIndividual=x;
-                //cout<<" x.fitness is= "<<x.fitness<<"\t";
             }
         }
         return bestIndividual;
@@ -238,11 +199,10 @@ class Genetic{
         }
         population.generateNewGeneration();
         Individual maxIndividual = population.selectBest();
-        cout<<"generation 1"<<" best solution fitness = "<<maxIndividual.fitness<<endl;
+
         for (int generation=0; generation<GENERATIONS -1; generation++){
             population.generateNewGeneration();
             Individual best = population.selectBest();
-            //cout<<"generation"<<generation+1<<best.fitness<<endl;
             if(best.fitness<maxIndividual.fitness){
                 maxIndividual=best;
             }
@@ -258,67 +218,8 @@ class Genetic{
 
 int main(){
     
-    /*ifstream myfile ("geneticTSP.txt");
-    string mystring;
-    if ( myfile.is_open() ) { // always check whether the file is open
-    myfile >> mystring; // pipe file's content into stream
-    cout << mystring; // pipe stream's content to standard output
-    }*/
     srand(time(0));
     Genetic ga;
     ga.run();
-    
-    
-    
-    /*Individual object1(NUM_NODES);
-    Individual object2(NUM_NODES);
-    
-    /*for (int i=0; i<NUM_NODES; i++){
-        //cout<<object1.nodes[i].x;
-        //cout<<object1.nodes[i].y<<endl;
-        for(int j=0; j<NUM_NODES; j++){
-            cout<<object1.distances[i][j]<<"\t";
-        }
-        cout<<endl;
-    }
-    //cout<<"before"<<object1.genes[0]<<endl;
-    object1.startNode(1);
-    object2.startNode(1);
-    //cout<<"after"<<object1.genes[0]<<endl;
-    object1.createRandomRoute();
-    object2.createRandomRoute();
-
-    //cout<<"before mutation"<<endl;
-
-  for (int i=0; i<NUM_NODES; i++){
-    cout<<object1.genes[i];
-    }
-    cout<<endl;
-    for (int i=0; i<NUM_NODES; i++){
-    cout<<object2.genes[i];
-    }
-
-
-    object1.crossover(object2);
-    object1.mutate();
-    //cout<<"fitness is"<<object1.fitness<<endl;
-  //cout<<"after mutation"<<endl;
-
-  for (int i=0; i<NUM_NODES; i++){
-    //cout<<object1.genes[i];
-    }
-
-
- 
-    
-    Population object3 (NUM_NODES);
-    object3.selectBest();
-    /*cout<<endl;
-    cout<<"test 1"<<endl;
-    cout<<"fitness"<<object3.individuals[0].fitness<<endl;
-    for(int i=0; i<POPULATION_SIZE; i++){
-        cout<<"the fitness of the "<<i<<"th individual is"<<object3.individuals[i].fitness;
-    }*/
-
     return 0;
 }
